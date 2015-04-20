84c10ccbba26fee9a770a123229468ea694d788f
luis rodriguez <luis.rodriguez@cru.org>
2015-04-20 10:20:57 -0400

    add kitchen file

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,5 @@ maintainer_email 'luis.rodriguez@cru.org'
 license          'All rights reserved'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 version          '0.1.0'
