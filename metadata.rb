af1a14839c43ced05bddfa2fbc0a9acc658148fc
luis rodriguez <luis.rodriguez@cru.org>
2015-04-21 22:24:58 -0400

    bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures wildfly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 #IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
-version          '0.1.0'
+version          '0.1.1'
 
 depends "apt"
 depends "java"
